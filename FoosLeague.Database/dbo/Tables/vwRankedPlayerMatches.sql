49074929cf8d453c027002b65b0cf216c293ec0e
g.sciarrino <g.sciarrino@xeel.tech>
2024-12-13 16:47:06 +0100

    #view ranked players

---
+++ FoosLeague.Database/dbo/Tables/vwRankedPlayerMatches.sql
@@ -0,0 +1,26 @@
+﻿CREATE VIEW [dbo].[vwRankedPlayerMatches]
+    AS 
+
+WITH RankedPlayers AS
+(
+    SELECT
+        MatchId,
+        Role,
+        PlayerId,
+        ScoreVariation,
+        ROW_NUMBER() OVER (PARTITION BY MatchId, Role, CASE WHEN ScoreVariation > 0 THEN 1 ELSE 0 END ORDER BY ScoreVariation DESC) AS Rank,
+        CASE 
+            WHEN ScoreVariation > 0 THEN 1  
+            ELSE 0                          
+        END AS IsWinner
+    FROM
+        dbo.PlayerMatches
+)
+SELECT MatchId,
+		Role,
+		PlayerId,
+		ScoreVariation,
+		IsWinner
+FROM RankedPlayers
+WHERE Rank = 1
+
