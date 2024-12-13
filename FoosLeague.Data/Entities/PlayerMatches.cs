0b60a7e255d0b8b234b2c987ecfc9eb7f3ef61cb
Matteo Conti <matteoco@basisgroup.it>
2024-12-13 15:39:16 +0100

    Entities

---
+++ FoosLeague.Data/Entities/PlayerMatches.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FoosLeague.Data.Entities;
+public class PlayerMatches
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public Guid PlayerId { get; set; } = Guid.NewGuid();
+    public Guid MatchId { get; set; } = Guid.NewGuid();
+    public int ScoreVariation { get; set; } = 0;
+    [Column(TypeName = "nvarchar(50)")]
+    public Role Role { get; set; } = Role.Forward;
+
+    public Player Player { get; set; }
+    public Match Match { get; set; }
+}
